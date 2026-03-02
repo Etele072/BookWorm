55a5d716ef25e1336349fb65dbcdedaa7f1af142
GrofRadayGedeon <mayeradam09@gmail.com>
2026-03-02 14:50:35 +0100

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, NavLink} from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 import Home from "./Home.jsx";
 import Register from './Register.jsx';
 import Login from "./Login.jsx";
-import './App.css'
+import Logo from "./BookwormLogo.png";
+import './App.css';
 
 function App() {
 
-const [loggedIn, setLoggedin] = useState(false);
+  const [loggedIn, setLoggedin] = useState(false);
 
-  useEffect(() =>{
-    if(localStorage.getItem("token")){
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
       setLoggedin(true);
     }
   }, []);
@@ -22,27 +23,39 @@ const [loggedIn, setLoggedin] = useState(false);
 
   return (
     <>
-    <BrowserRouter>
-    <nav>
-    <NavLink to="/Home">Kezdőlap</NavLink> | {" "}
-    {loggedIn ? (
-      <>
-      <button onClick={logout}>Kilépés</button>
-      </>
-    ):(
-      <>
-    <NavLink to="/Register">Regisztráció</NavLink> | {" "}
-    <NavLink to="/Login">Bejelentkezés</NavLink>
-      </>
-    )}
+      <BrowserRouter>
+        <nav className='navbar'>
+          <div className='logo-container'>
+            <img src={Logo} alt="Bookwormlogo" className="navlogo" />
+          </div>
 
-    </nav>
-    <Routes>
-      <Route path="/Home" element={<Home/>} />
-      <Route path="/Register" element={<Register/>}/>
-      <Route path="/Login" element={<Login setLoggedin={setLoggedin}/>}/>
-    </Routes>
-    </BrowserRouter>
+          <div className='navlinks'>
+            <NavLink to="/Home">Kezdőlap</NavLink>
+            <NavLink to="/Aboutus">Rólunk</NavLink>
+            {loggedIn ? (
+              <>
+
+                <NavLink to="/Profile">Profil</NavLink>
+                <NavLink to="/Favourites">Kedvencek</NavLink>
+                <NavLink to="/Orders">Rendelések</NavLink>
+                <NavLink to="/Cart">Kosár</NavLink>
+                <button onClick={logout} className='navbutton'>Kilépés</button>
+              </>
+            ) : (
+              <>
+
+                <NavLink to="/Register">Regisztráció</NavLink>
+                <NavLink to="/Login">Bejelentkezés</NavLink>
+              </>
+            )}
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login setLoggedin={setLoggedin} />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
